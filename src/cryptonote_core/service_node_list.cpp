281377b6a41244f1c851bfd2040b8495cbddd031
doy-lee <doylet@protonmail.com>
2018-09-27 13:32:31 +1000

    On clear, start height from hardfork 9 height.

--- src/cryptonote_core/service_node_list.cpp
+++ src/cryptonote_core/service_node_list.cpp
@@ -1108,7 +1108,14 @@ namespace service_nodes
     }
 
     m_quorum_states.clear();
-    m_height = 0;
+
+    uint64_t hardfork_9_from_height = 0;
+    {
+      uint32_t window, votes, threshold;
+      uint8_t voting;
+      m_blockchain.get_hard_fork_voting_info(9, window, votes, threshold, hardfork_9_from_height, voting);
+    }
+    m_height = hardfork_9_from_height;
   }
 
   bool convert_registration_args(cryptonote::network_type nettype, std::vector<std::string> args, std::vector<cryptonote::account_public_address>& addresses, std::vector<uint64_t>& portions, uint64_t& portions_for_operator, bool& autostake)
