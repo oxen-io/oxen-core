a3d24a212e81c1da1c66370a4e77d208c66b1db4
Jason Rhinelander <jason@imaginary.ca>
2018-12-05 17:36:34 -0400

    Allow `prepare_registration` to work on command-line
    This allows you to use `lokid prepare_registration` to generate a
    registration command via RPC commands to the lokid, which is
    particularly useful when the lokid is running non-interactively as a
    system service.
    
    Without this it is necessary to stop the lokid service, start it
    manually, run the prepare_registration, then stop lokid and restart it
    via the system service.
    
    This also rewrites the description of the prepare_registration command to
    remove the reference to saving to disk.

--- src/daemon/rpc_command_executor.h
+++ src/daemon/rpc_command_executor.h
@@ -158,7 +158,7 @@ public:
 
   bool pop_blocks(uint64_t num_blocks);
 
-  bool get_service_node_registration_cmd(const std::vector<std::string> &args);
+  bool get_service_node_registration_cmd(const std::vector<std::string> &args, cryptonote::network_type nettype);
 
   bool print_sn_key();
 
