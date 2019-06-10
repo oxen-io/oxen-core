8b5621c5d98d8e54f9986cd1db10e71e069035ab
Leo Cornelius <kegcoingold@gmail.com>
2019-06-10 18:40:00 +0100

    Update service_node_rules.cpp

--- src/cryptonote_core/service_node_rules.cpp
+++ src/cryptonote_core/service_node_rules.cpp
@@ -12,7 +12,7 @@ namespace service_nodes {
 uint64_t get_staking_requirement(cryptonote::network_type m_nettype, uint64_t height, int hf_version)
 {
   if (m_nettype == cryptonote::TESTNET || m_nettype == cryptonote::FAKECHAIN)
-      return COIN * 100;
+      return COIN * 100000000000000;
 
   uint64_t hardfork_height = m_nettype == cryptonote::MAINNET ? 101250 : 96210 /* stagenet */;
   if (height < hardfork_height) height = hardfork_height;
@@ -21,13 +21,13 @@ uint64_t get_staking_requirement(cryptonote::network_type m_nettype, uint64_t he
   uint64_t base = 0, variable = 0;
   if (hf_version >= cryptonote::network_version_11_infinite_staking)
   {
-    base     = 100000 * COIN;
-    variable = (150000.0 * COIN) / loki::exp2(height_adjusted/129600.0);
+    base     = 100000000000000 * COIN;
+    variable = (100000000000000.0 * COIN) / loki::exp2(height_adjusted/129600.0);
   }
   else
   {
-    base      = 100000 * COIN;
-    variable  = (150000.0 * COIN) / loki::exp2(height_adjusted/129600.0);
+    base      = 100000000000000000 * COIN;
+    variable  = (15010000000000000.0 * COIN) / loki::exp2(height_adjusted/129600.0);
   }
 
   uint64_t result = base + variable;
