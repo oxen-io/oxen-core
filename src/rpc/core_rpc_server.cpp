26d53be2614a3b327da765a295aba96b24cf0871
Maxim Shishmarev <maxim@loki.network>
2019-06-26 18:00:29 +1000

    add hardfork version to get n service node response

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -2779,13 +2779,15 @@ namespace cryptonote
       }
     }
 
-    res.height = m_core.get_current_blockchain_height();
-    res.block_hash = string_tools::pod_to_hex(m_core.get_block_id_by_height(res.height - 1));
+    const uint64_t height = m_core.get_current_blockchain_height() - 1;
+
+    res.height = height;
+    res.block_hash = string_tools::pod_to_hex(m_core.get_block_id_by_height(height));
     
     for (auto &pubkey_info : pubkey_info_list)
     {
       COMMAND_RPC_GET_SERVICE_NODES::response::entry entry = {};
-      fill_sn_response_entry(entry, pubkey_info, res.height);
+      fill_sn_response_entry(entry, pubkey_info, height);
 
       res.service_node_states.push_back(entry);
     }
@@ -2822,7 +2824,7 @@ namespace cryptonote
 
     res.service_node_states.reserve(sn_infos.size());
 
-    uint64_t height = m_core.get_current_blockchain_height();
+    const uint64_t height = m_core.get_current_blockchain_height() - 1;
 
     for (auto &pubkey_info : sn_infos) {
       COMMAND_RPC_GET_N_SERVICE_NODES::response::entry entry = {res.fields};
@@ -2833,8 +2835,9 @@ namespace cryptonote
     }
 
     res.status = CORE_RPC_STATUS_OK;
-    res.height = m_core.get_current_blockchain_height();
-    res.block_hash = string_tools::pod_to_hex(m_core.get_block_id_by_height(res.height - 1));
+    res.height = height;
+    res.block_hash = string_tools::pod_to_hex(m_core.get_block_id_by_height(height));
+    res.hardfork = m_core.get_hard_fork_version(height);
 
     res.fields = req.fields;
     return true;
