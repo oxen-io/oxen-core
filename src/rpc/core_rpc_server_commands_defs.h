a508a651b04aade6b753caf79e9701322aee58d2
Jason Rhinelander <jason@imaginary.ca>
2019-06-30 23:24:15 -0300

    Add `funded` boolean to sn status response

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -2781,7 +2781,8 @@ namespace cryptonote
         uint64_t                  last_reward_block_height;      // The last height at which this Service Node received a reward.
         uint32_t                  last_reward_transaction_index; // When multiple Service Nodes register on the same height, the order the transaction arrive dictate the order you receive rewards.
         uint64_t                  last_uptime_proof;             // The last time this Service Node's uptime proof was relayed by at least 1 Service Node other than itself in unix epoch time.
-        bool                      active;                        // True if fully funded and not currently decommissioned
+        bool                      active;                        // True if fully funded and not currently decommissioned (and so `active && !funded` implicitly defines decommissioned)
+        bool                      funded;                        // True if the required stakes have been submitted to activate this Service Node
         uint64_t                  state_height;                  // If active: the state at which registration was completed; if decommissioned: the decommissioning height; if awaiting: the last contribution (or registration) height
         uint32_t                  decommission_count;            // The number of times the Service Node has been decommissioned since registration
         int64_t                   earned_downtime_blocks;        // The number of blocks earned towards decommissioning, or the number of blocks remaining until deregistration if currently decommissioned
@@ -2804,6 +2805,7 @@ namespace cryptonote
             KV_SERIALIZE(last_reward_transaction_index)
             KV_SERIALIZE(last_uptime_proof)
             KV_SERIALIZE(active)
+            KV_SERIALIZE(funded)
             KV_SERIALIZE(state_height)
             KV_SERIALIZE(decommission_count)
             KV_SERIALIZE(earned_downtime_blocks)
@@ -2859,6 +2861,7 @@ namespace cryptonote
       bool last_reward_transaction_index;
       bool last_uptime_proof;
       bool active;
+      bool funded;
       bool state_height;
       bool decommission_count;
       bool earned_downtime_blocks;
@@ -2886,6 +2889,7 @@ namespace cryptonote
       KV_SERIALIZE_OPT2(last_reward_transaction_index, false)
       KV_SERIALIZE_OPT2(last_uptime_proof, false)
       KV_SERIALIZE_OPT2(active, false)
+      KV_SERIALIZE_OPT2(funded, false)
       KV_SERIALIZE_OPT2(state_height, false)
       KV_SERIALIZE_OPT2(decommission_count, false)
       KV_SERIALIZE_OPT2(earned_downtime_blocks, false)
@@ -2938,7 +2942,8 @@ namespace cryptonote
         uint64_t                  last_reward_block_height;      // The last height at which this Service Node received a reward.
         uint32_t                  last_reward_transaction_index; // When multiple Service Nodes register on the same height, the order the transaction arrive dictate the order you receive rewards.
         uint64_t                  last_uptime_proof;             // The last time this Service Node's uptime proof was relayed by atleast 1 Service Node other than itself in unix epoch time.
-        bool                      active;                        // True if fully funded and not currently decommissioned
+        bool                      active;                        // True if fully funded and not currently decommissioned (and so `active && !funded` implicitly defines decommissioned)
+        bool                      funded;                        // True if the required stakes have been submitted to activate this Service Node
         uint64_t                  state_height;                  // If active: the state at which registration was completed; if decommissioned: the decommissioning height; if awaiting: the last contribution (or registration) height
         uint32_t                  decommission_count;            // The number of times the Service Node has been decommissioned since registration
         int64_t                   earned_downtime_blocks;        // The number of blocks earned towards decommissioning, or the number of blocks remaining until deregistration if currently decommissioned
@@ -2961,6 +2966,7 @@ namespace cryptonote
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(last_reward_transaction_index);
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(last_uptime_proof);
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(active);
+          KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(funded);
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(state_height);
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(decommission_count);
           KV_SERIALIZE_ENTRY_FIELD_IF_REQUESTED(earned_downtime_blocks);
