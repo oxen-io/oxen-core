1324bebcb036f9403d2c6b0ac348328c931e90a4
Doyle <doylet@protonmail.com>
2019-07-10 12:20:41 +1000

    Gate difficulty workaround to only be on mainnet

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1062,11 +1062,14 @@ bool Blockchain::switch_to_alternative_blockchain(std::list<block_extended_info>
   // TODO(loki): This is a work around for sometimes reorganising the blockchain causing inconsistent difficulty values
   LOKI_DEFER
   {
-    uint64_t const FUDGE                             = 60;
-    cryptonote::BlockchainDB::fixup_context context  = {};
-    context.type                                     = cryptonote::BlockchainDB::fixup_type::calculate_difficulty;
-    context.calculate_difficulty_params.start_height = split_height < FUDGE ? 0 : split_height - FUDGE;
-    m_db->fixup(context);
+    if (nettype() == MAINNET)
+    {
+      uint64_t const FUDGE                             = 60;
+      cryptonote::BlockchainDB::fixup_context context  = {};
+      context.type                                     = cryptonote::BlockchainDB::fixup_type::calculate_difficulty;
+      context.calculate_difficulty_params.start_height = split_height < FUDGE ? 0 : split_height - FUDGE;
+      m_db->fixup(context);
+    }
   };
 
   for (BlockchainDetachedHook* hook : m_blockchain_detached_hooks)
