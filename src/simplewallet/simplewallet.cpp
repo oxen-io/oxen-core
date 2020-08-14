110637aea1e3a170dbc848aa02402a575c24ce0f
Ghulame Mujtaba Arbi <58784608+steelslayer091@users.noreply.github.com>
2020-08-15 00:57:00 +0200

    show_transfers displays column headers
    Formatting changes

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -8256,7 +8256,7 @@ bool simple_wallet::show_transfers(const std::vector<std::string> &args_)
   
   auto formatter = boost::format("%8.8s %6.6s %8.8s %12.12s %16.16s %20.20s %64s %16s %14.14s %s %s - %s");
  
-  message_writer(color, true) << formatter
+  message_writer(epee::console_color_white, true) << formatter
       % "Height"
       % "Type"
       % "Locked"
