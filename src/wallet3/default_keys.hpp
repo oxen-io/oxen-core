407cc8e18765289846fe0193320b35e5ea71c87a
Thomas Winget <tewinget@gmail.com>
2021-08-31 14:31:51 -0400

    remove bad abstraction idea

--- src/wallet3/default_keys.hpp
+++ src/wallet3/default_keys.hpp
@@ -14,15 +14,7 @@ namespace wallet
 
   public:
 
-    DBKeys() = delete;
-
-    DBKeys(std::function<bytestring_view()> private_spendkey_cb = nullptr,
-           bytestring_view public_spend_key,
-           bytestring_view private_view_key,
-           bytestring_view private_spend_key);
-
-  private:
-
-    std::function<bytestring_view()> GetSpendKey;
+    DBKeys();
+  };
 
 } // namespace wallet
