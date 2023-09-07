baecf82a3bb6a674facc5b6a8400f76432e6f4c4
saravanan <95636211+saravananp-001@users.noreply.github.com>
2023-09-07 11:28:09 +0530

    compilation fix for mapping name_hash
    error: 'const class nlohmann::basic_json<>' has no member named 'name_hash'
    
     7199 |                 mapping.name_hash};
    
    resolved this error by replacing  mapping.name_hash with  mapping["name_hash"]

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -7196,7 +7196,7 @@ bool simple_wallet::ons_lookup(std::vector<std::string> args) {
         tools::wallet2::ons_detail detail = {
                 static_cast<ons::mapping_type>(mapping["type"]),
                 name,
-                mapping.name_hash};
+                mapping["name_hash"]};
         m_wallet->set_ons_cache_record(detail);
     }
     for (size_t i = last_index + 1; i < args.size(); i++)
