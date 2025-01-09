a3a3177f47fefc566d0433f854156b8c737b639e
doylet <git.doylet.6tnrz@simplelogin.com>
2025-01-09 16:43:43 +1100

    Handle MSYS MinGW compiler which does not have a posix suffixed executable

--- cmake/64-bit-toolchain.cmake
+++ cmake/64-bit-toolchain.cmake
@@ -26,14 +26,22 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-if (NOT CMAKE_HOST_WIN32)
+if (CMAKE_HOST_WIN32)
+  # Building w/ MSYS MinGW as recommended by the readme only ships MinGW with
+  # the posix threading API as confirmed by 'x86_64-w64-mingw32-gcc.exe -v'
+  #  > Thread model: posix
+  set (CMAKE_C_COMPILER ${ARCH_TRIPLET}-gcc)
+  set (CMAKE_CXX_COMPILER ${ARCH_TRIPLET}-g++)
+else()
+  # Our cross-compiler ships MinGW GCC with a 'posix' suffix to indicate a build
+  # with the posix threading model.
   set (CMAKE_SYSTEM_NAME Windows)
+  set (CMAKE_C_COMPILER ${ARCH_TRIPLET}-gcc-posix)
+  set (CMAKE_CXX_COMPILER ${ARCH_TRIPLET}-g++-posix)
 endif()
 
 set (ARCH_TRIPLET x86_64-w64-mingw32)
 set (CMAKE_SYSTEM_PROCESSOR x86_64)
-set (CMAKE_C_COMPILER ${ARCH_TRIPLET}-gcc-posix)
-set (CMAKE_CXX_COMPILER ${ARCH_TRIPLET}-g++-posix)
 set (CMAKE_AR ar CACHE FILEPATH "" FORCE)
 set (CMAKE_NM nm CACHE FILEPATH "" FORCE)
 set (CMAKE_LINKER ld CACHE FILEPATH "" FORCE)
